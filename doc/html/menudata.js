deeea23dbe930ce2fa6302e0fe3c2ded095fb2fb
ahmetemregurdal <ahmetemregurdal2008@gmail.com>
2024-01-03 15:40:22 +0300

    Finished documenting the current functions

--- doc/html/menudata.js
+++ doc/html/menudata.js
@@ -29,7 +29,11 @@ var menudata={children:[
 {text:"Class List",url:"annotated.html"},
 {text:"Class Index",url:"classes.html"},
 {text:"Class Members",url:"functions.html",children:[
-{text:"All",url:"functions.html"},
-{text:"Functions",url:"functions_func.html"}]}]},
+{text:"All",url:"functions.html",children:[
+{text:"o",url:"functions.html#index_o"},
+{text:"u",url:"functions.html#index_u"}]},
+{text:"Functions",url:"functions_func.html",children:[
+{text:"o",url:"functions_func.html#index_o"},
+{text:"u",url:"functions_func.html#index_u"}]}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"}]}]}
