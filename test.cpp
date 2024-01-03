02ffcb60005b52c4c96c1fe504b105853333f7e7
ahmetemregurdal <ahmetemregurdal2008@gmail.com>
2024-01-03 23:48:20 +0300

    Removed some accidental files

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-#include "include/int23_t.hpp"
-
-int main() {
-	int23_t::int23_t abc(5);
-	std::cout<<abc<<'\n';
-	std::cout<<int23_t::int23_t(-1).num.to_string()<<'\n';
-	std::cout << (abc * int23_t::int23_t(-1)).num.to_string() << '\n';
-}
